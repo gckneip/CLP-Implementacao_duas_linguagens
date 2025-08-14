af79c553c68da6131234a4d98745dca834744f7a
mthsrnn <mrffreitas@inf.ufpel.edu.br>
2025-08-14 18:55:45 -0300

    gameplay de matilha implementado

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,9 @@
+mod jogo;
 use pyo3::prelude::*;
-use std::thread;
-use std::time::Duration;
-
-#[pyfunction]
-fn do_long_task() -> PyResult<String> {
-    println!("Iniciando tarefa longa em Rust...");
-    thread::sleep(Duration::from_secs(3)); 
-    println!("Tarefa longa concluída!");
-    Ok("Tarefa concluída com sucesso!".to_string())
-}
-
+use jogo::Jogo;
 
 #[pymodule]
-fn pintada(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(do_long_task))?;
+fn pintada(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Jogo>()?;
     Ok(())
 }
